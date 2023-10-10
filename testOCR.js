d96a56b3336ad8ba518579d0e8dac33f1fe2de63
Alex Pedersen <pedersen@college.harvard.edu>
2023-10-09 21:39:37 -0700

    switch hosts

--- testOCR.js
+++ testOCR.js
@@ -6,8 +6,8 @@ import {
 } from "./index.js";
 import "dotenv/config";
 import fs from "fs";
-const s3DownloadUrlPrefix =
-  "https://f155-18-117-160-75.ngrok-free.app/downloadDocument?key=";
+// const s3DownloadUrlPrefix =
+//   "https://f155-18-117-160-75.ngrok-free.app/downloadDocument?key=";
 const chineseInvoiceS3Id = "9bcf6719-b3d0-4ae4-a0cf-50f93b0dfb0c.pdf";
 const receiptPhotoS3Id = "fd07426a-1961-4078-a6a5-790751f462ca.jpg";
 const johnsPorfolio = "2758c016-51f2-40d5-9241-e7f289ccfb8b.pdf";
